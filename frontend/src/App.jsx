6d8b57a2a007a4587afe083d3a2fd789ffe74810
metisssi <simcheraadnrij@gmail.com>
2025-10-25 10:21:46 +0200

    dsd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
-import OnboardingPage from "./pages/OnboardingPage.jsx";
+import OnboardingPage from "./pages/OnBoardingPage.jsx";
 
 import { Toaster } from "react-hot-toast";
 
